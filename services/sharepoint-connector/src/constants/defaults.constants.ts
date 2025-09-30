215f6aeabfe1105c7f2826c9257b1ca25a70bb79
Lorand Sandor <lorand@Lorands-MacBook-Pro.local>
2025-09-30 12:41:37 +0300

    chore(sharepoint-connector): deleted outdated file

--- services/sharepoint-connector/src/constants/defaults.constants.ts
+++ services/sharepoint-connector/src/constants/defaults.constants.ts
@@ -4,8 +4,6 @@ export const DEFAULT_MAX_FILE_SIZE_BYTES = 209715200; // 200MB
 
 export const DEFAULT_DOWNLOAD_LOG_INTERVAL_BYTES = 1000000; // 1MB
 
-export const DEFAULT_TOKEN_EXPIRATION_BUFFER_MS = 5 * 60 * 1000; // 5 minutes
-
 export const HTTP_STATUS_OK_MAX = 299;
 
 export const DEFAULT_MIME_TYPE = 'application/octet-stream';
