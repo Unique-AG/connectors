8cca58a307e61c7867274e814f2e22b91585713e
Lorand Sandor <lorand@Lorands-MacBook-Pro.local>
2025-09-30 12:45:09 +0300

    chore(sharepoint-connector): replaced console.log with logger.log in main.ts file

--- services/sharepoint-connector/src/main.ts
+++ services/sharepoint-connector/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
 
   const port = configService.get('app.port', { infer: true });
   await app.listen(port);
-  console.log(`Server is running on http://localhost:${port}`);
+  logger.log(`Server is running on http://localhost:${port}`);
 }
 
 initOpenTelemetry({
