644a1a4d4a9ec66fef8694d2fa0fe4c685f6efe6
Konstantin Krauss <konsti@users.noreply.github.com>
2025-10-10 09:17:26 +0200

    feat: working email sync

---
+++ services/agentic-outlook-mcp/src/amqp/amqp.module.ts
@@ -0,0 +1,63 @@
+import { RabbitMQModule } from "@golevelup/nestjs-rabbitmq";
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { AppConfig, AppSettings } from "../app-settings";
+
+@Module({
+  imports: [RabbitMQModule.forRootAsync({
+    useFactory: (configService: ConfigService<AppConfig, true>) => ({
+      uri: configService.get(AppSettings.AMQP_URL),
+      connectionInitOptions: {
+        wait: false,
+      },
+      exchanges: [
+        { name: 'email.pipeline', type: 'topic', options: { durable: true } },
+        { name: 'email.pipeline.retry', type: 'topic', options: { durable: true } },
+        { name: 'email.pipeline.dlx', type: 'topic', options: { durable: true } },
+      ],
+      queues: [
+        {
+          name: 'q.email.ingest',
+          exchange: 'email.pipeline',
+          routingKey: 'email.ingest',
+          options: { durable: true },
+        },
+        {
+          name: 'q.email.ingest.retry',
+          exchange: 'email.pipeline.retry',
+          routingKey: 'email.ingest.retry',
+          options: {
+            durable: true,
+            deadLetterExchange: 'email.pipeline',
+            deadLetterRoutingKey: 'email.ingest',
+          },
+        },
+        {
+          name: 'q.email.ingest.dlx',
+          exchange: 'email.pipeline.dlx',
+          routingKey: 'email.ingest.dlx',
+          options: { durable: true },
+        },
+        // { name: 'q.email.process' },
+        // { name: 'q.email.process.retry' },
+        // { name: 'q.email.process.dlx' },
+        // { name: 'q.email.embed' },
+        // { name: 'q.email.embed.retry' },
+        // { name: 'q.email.embed.dlx' },
+        // { name: 'q.email.index' },
+        // { name: 'q.email.index.retry' },
+        // { name: 'q.email.index.dlx' },
+      ],
+      channels: {
+        pipeline: {
+          prefetchCount: 20,
+          default: true,
+        },
+      },
+    }),
+    inject: [ConfigService],
+  }),],
+  providers: [],
+  exports: [RabbitMQModule],
+})
+export class AmqpModule {}
