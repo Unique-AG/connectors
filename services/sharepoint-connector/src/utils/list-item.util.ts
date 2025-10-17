a9bc0e509dc7e416e5468b720b90413c18b8af5e
Lorand Sandor <lorand@Lorands-MacBook-Pro.local>
2025-10-17 16:41:47 +0300

    chore: added undefined check for fields.FileLeafRef

--- services/sharepoint-connector/src/utils/list-item.util.ts
+++ services/sharepoint-connector/src/utils/list-item.util.ts
@@ -4,7 +4,7 @@ export function getTitle(fields: ListItem['fields']): string {
   const title = fields.Title?.trim();
   // The title sometimes comes back as default value Title, even though the file leaf ref has a valid name. Because of this we fallback to the file leaf ref instead of displaying the default value.
   // FileLeafRef is the file name
-  if (!title || title.toLowerCase() === 'title') {
+  if (fields.FileLeafRef && (!title || title.toLowerCase() === 'title')) {
     return fields.FileLeafRef;
   }
 
