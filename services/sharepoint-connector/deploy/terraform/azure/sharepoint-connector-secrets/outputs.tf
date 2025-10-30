87e52432f63bb31bbdbfe53ddc0c171228f67fe0
unique-dominik <dominik@unique.ch>
2025-10-30 14:09:37 +0100

    feat(sharepoint-connector): add certificate support

--- services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-secrets/outputs.tf
+++ services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-secrets/outputs.tf
@@ -3,12 +3,8 @@ output "entra_certificate_0" {
   description = "The Entra certificate for the SharePoint connector. Values must be passed to an Entra Application (Certificate Secret) or azuread_application_certificate."
   value = var.entra_application_certificate_0 != null ? {
     certificate_pem = tls_self_signed_cert.entra_certificate_0[0].cert_pem
-    private_key_pem = tls_private_key.entra_certificate_0[0].private_key_pem
     end_date        = tls_self_signed_cert.entra_certificate_0[0].validity_end_time
     start_date      = tls_self_signed_cert.entra_certificate_0[0].validity_start_time
-    name            = try(var.entra_application_certificate_0.name, "spc-entra-app-certificate-0")
-    version         = try(var.entra_application_certificate_0.version, 0)
-    rotation_id     = random_id.certificate_rotation[0].hex
   } : null
 }
 
