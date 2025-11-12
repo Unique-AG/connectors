ae6c2abe6f849e60f7c4be20b3945d7c709d4e20
unique-dominik <dominik@unique.ch>
2025-11-12 10:45:04 +0100

    feat(sharepoint-connector): allow auto-creating key and certificate

--- services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-entra-application/outputs.tf
+++ services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-entra-application/outputs.tf
@@ -2,3 +2,7 @@ output "client_id" {
   description = "This ID combined with a Tenant ID will result in a valid workload/managed identity."
   value       = azuread_application.sharepoint_connector.client_id
 }
+output "object_id" {
+  description = "The object ID of the Azure AD service principal."
+  value       = azuread_service_principal.sharepoint_connector.object_id
+}
