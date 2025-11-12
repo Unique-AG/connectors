3f11b45c067ea3f7b8eb14c17ee856c51e0cc096
unique-dominik <dominik@unique.ch>
2025-11-12 14:07:36 +0100

    feat(sharepoint-connector): allow auto-creating key and certificate

--- services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-secrets/outputs.tf
+++ services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-secrets/outputs.tf
@@ -1,8 +1,8 @@
 output "certificate" {
   description = "The PEM-encoded TLS certificate (public part). Null if certificate generation is disabled."
   value = var.tls_certificate != null ? {
-    pem             = tls_self_signed_cert.certificate[0].cert_pem
-    expiration_date = tls_self_signed_cert.certificate[0].not_after
+    pem               = tls_self_signed_cert.certificate[0].cert_pem
+    validity_end_time = tls_self_signed_cert.certificate[0].validity_end_time
   } : null
 }
 
