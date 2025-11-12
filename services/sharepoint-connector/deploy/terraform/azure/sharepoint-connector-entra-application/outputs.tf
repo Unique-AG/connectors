0bb5b95326cbd5b763bea2beb81664f8d7bf525a
Dominik from Unique <dominik@unique.ch>
2025-11-12 15:43:24 +0100

    feat(sharepoint-connector): allow auto-creating key and certificate (#104)
    ## Risk Triage
    This will put the key into the terraform state. 
    
    ### Internal mitigations
    
    The key appears in the state.
    
    - Use `ephemeral` resource - does not work as the `certificate` does not
    allow `wo` `ephemeral` input
    - Make certificate creation optional / opt-in - done
    - Notify users in variables - done
    - Offer rotation - done
    
    Further risk considerations: In most companies (incl. Unique) the attack
    vector to reach the terraform state is more complex than reaching the
    Key Vault itself (where the Key ends up). This is due to the fact that
    Support teams support clients and potentially debug an issue. Support
    teams never need to access the terraform state itself for their daily
    work.
    
    ### External Mitigations
    Users can use `Named Locations` and `Conditional Access Policies` to
    lock the use of the certificate to a source IP-Range or Location.
    
    ## Note on rotation
    **Will there be downtime?** Not if its pre-expire because the pods will
    still know the old key until rebooted.
    Real, as code rotation is something that can be tackled in future
    releases.
    
    Refs: UN-14890

--- services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-entra-application/outputs.tf
+++ services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-entra-application/outputs.tf
@@ -2,3 +2,7 @@ output "client_id" {
   description = "This ID combined with a Tenant ID will result in a valid workload/managed identity."
   value       = azuread_application.sharepoint_connector.client_id
 }
+output "object_id" {
+  description = "The object ID of the Azure AD service principal. Null if service principal was not created."
+  value       = var.service_principal_configuration != null ? azuread_service_principal.sharepoint_connector[0].object_id : null
+}
