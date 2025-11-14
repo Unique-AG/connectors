7072e3f4173b03b5a44dd24e83ddd925f87de84d
Konstantin Krauss <konsti@users.noreply.github.com>
2025-11-14 15:44:47 -0500

    feat: add fork of temporal package

---
+++ packages/temporal/src/decorators/activities.decorator.ts
@@ -0,0 +1,35 @@
+/** biome-ignore-all lint/complexity/noBannedTypes: Fork of KurtzL/nestjs-temporal */
+/** biome-ignore-all lint/suspicious/noExplicitAny: Fork of KurtzL/nestjs-temporal */
+import { Scope, SetMetadata } from '@nestjs/common';
+import { SCOPE_OPTIONS_METADATA } from '@nestjs/common/constants';
+import { ActivityOptions } from '@temporalio/workflow';
+import { TEMPORAL_MODULE_ACTIVITIES } from '../temporal.constants';
+
+export interface ActivitiesOptions extends ActivityOptions {
+  /**
+   * Specifies the name of the queue to subscribe to.
+   */
+  name?: string;
+  /**
+   * Specifies the lifetime of an injected Activities.
+   */
+  scope?: Scope;
+}
+
+export function Activities(): ClassDecorator;
+export function Activities(queueName: string): ClassDecorator;
+export function Activities(
+  activitiesOptions: ActivitiesOptions,
+): ClassDecorator;
+export function Activities(
+  queueNameOrOptions?: string | ActivitiesOptions,
+): ClassDecorator {
+  const options =
+    queueNameOrOptions && typeof queueNameOrOptions === 'object'
+      ? queueNameOrOptions
+      : { name: queueNameOrOptions };
+  return (target: any) => {
+    SetMetadata(SCOPE_OPTIONS_METADATA, options)(target);
+    SetMetadata(TEMPORAL_MODULE_ACTIVITIES, options)(target);
+  };
+}
---
+++ packages/temporal/src/temporal.module.ts
@@ -0,0 +1,87 @@
+/** biome-ignore-all lint/complexity/noThisInStatic: Fork of KurtzL/nestjs-temporal */
+import { DynamicModule, Module } from '@nestjs/common';
+import { DiscoveryModule } from '@nestjs/core';
+import {
+  SharedWorkflowClientOptions,
+  TemporalModuleOptions,
+} from './interfaces';
+import { TemporalExplorer } from './temporal.explorer';
+import {
+  ConfigurableModuleClass,
+  TEMPORAL_MODULE_ASYNC_OPTIONS_TYPE,
+  TEMPORAL_MODULE_OPTIONS_TYPE,
+} from './temporal.module-definition';
+import { createClientProviders } from './temporal.providers';
+import { TemporalMetadataAccessor } from './temporal-metadata.accessors';
+import { createClientAsyncProvider } from './utils';
+
+@Module({})
+export class TemporalModule extends ConfigurableModuleClass {
+  /**
+   * Create a new Temporal worker.
+   *
+   * @deprecated Use registerWorker.
+   */
+  public static forRoot(options: typeof TEMPORAL_MODULE_OPTIONS_TYPE): DynamicModule {
+    return TemporalModule.registerWorker(options);
+  }
+
+  /**
+   * Create a new Temporal worker.
+   *
+   * @deprecated Use registerWorker.
+   */
+  public static forRootAsync(
+    options: typeof TEMPORAL_MODULE_ASYNC_OPTIONS_TYPE,
+  ): DynamicModule {
+    return TemporalModule.registerWorkerAsync(options);
+  }
+
+  public static registerWorker(
+    options: typeof TEMPORAL_MODULE_OPTIONS_TYPE,
+  ): DynamicModule {
+    const superDynamicModule = super.registerWorker(options);
+    superDynamicModule.imports = [DiscoveryModule];
+    superDynamicModule.providers?.push(
+      TemporalExplorer,
+      TemporalMetadataAccessor,
+    );
+    return superDynamicModule;
+  }
+
+  public static registerWorkerAsync(
+    options: typeof TEMPORAL_MODULE_ASYNC_OPTIONS_TYPE,
+  ): DynamicModule {
+    const superDynamicModule = super.registerWorkerAsync(options);
+    superDynamicModule.imports?.push(DiscoveryModule);
+    superDynamicModule.providers?.push(
+      TemporalExplorer,
+      TemporalMetadataAccessor,
+    );
+    return superDynamicModule;
+  }
+
+  public static registerClient(options?: TemporalModuleOptions): DynamicModule {
+    const createClientProvider = createClientProviders(options ? [options] : []);
+    return {
+      global: true,
+      module: TemporalModule,
+      providers: createClientProvider,
+      exports: createClientProvider,
+    };
+  }
+  public static registerClientAsync(
+    asyncSharedWorkflowClientOptions: SharedWorkflowClientOptions,
+  ): DynamicModule {
+    const providers = createClientAsyncProvider(
+      asyncSharedWorkflowClientOptions,
+    );
+
+    return {
+      global: true,
+      module: TemporalModule,
+      providers,
+      exports: providers,
+    };
+  }
+}
