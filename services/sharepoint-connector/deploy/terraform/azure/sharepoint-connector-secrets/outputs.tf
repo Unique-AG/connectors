91d7acbb2a56492dd30ce6ab453e5f58e917bc89
Dominik from Unique <dominik@unique.ch>
2025-11-18 15:13:25 +0100

    fix(sharepoint-connector): preformat terraform output (#117)
    Refs: UN-15036

--- services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-secrets/outputs.tf
+++ services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-secrets/outputs.tf
@@ -2,7 +2,7 @@ output "certificate" {
   description = "The PEM-encoded TLS certificate (public part). Null if certificate generation is disabled."
   value = var.tls_certificate != null ? {
     pem               = tls_self_signed_cert.certificate[0].cert_pem
-    validity_end_time = tls_self_signed_cert.certificate[0].validity_end_time
+    validity_end_time = formatdate("YYYY-MM-DD'T'hh:mm:ss'Z'", tls_self_signed_cert.certificate[0].validity_end_time)
   } : null
 }
 
