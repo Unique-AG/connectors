f2b6dd9fc17badab1887b8fdd69bc1487e9e1580
Michał Simka <michal.simka@unique.ch>
2025-11-28 14:54:52 +0100

    Re-throw error for nestjs to catch up on it

--- services/sharepoint-connector/src/sharepoint-synchronization/sharepoint-synchronization.service.ts
+++ services/sharepoint-connector/src/sharepoint-synchronization/sharepoint-synchronization.service.ts
@@ -184,6 +184,7 @@ export class SharepointSynchronizationService {
         result: 'failure',
         failure_step: 'unknown',
       });
+      throw error;
     } finally {
       this.isScanning = false;
     }
