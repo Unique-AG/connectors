bf7ec6e81256c5cc7e5f630a6d63a9cc78c0edfd
Lorand Sandor <lorand@Lorands-MacBook-Pro.local>
2025-11-28 10:48:23 +0200

    refact: removed log on queue for bottleneck instances

--- services/sharepoint-connector/src/utils/bottleneck.factory.ts
+++ services/sharepoint-connector/src/utils/bottleneck.factory.ts
@@ -1,7 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import Bottleneck from 'bottleneck';
-import { Config } from '../config';
 
 export interface BottleneckConfig {
   reservoir: number;
@@ -14,11 +12,8 @@ export interface BottleneckConfig {
 @Injectable()
 export class BottleneckFactory {
   private readonly logger = new Logger(this.constructor.name);
-  private readonly isDebugEnabled: boolean;
 
-  public constructor(private readonly configService: ConfigService<Config, true>) {
-    this.isDebugEnabled = this.configService.get('app.logLevel', { infer: true }) === 'debug';
-  }
+  public constructor() {}
 
   public createLimiter(config: BottleneckConfig, contextName: string): Bottleneck {
     const limiter = new Bottleneck(config);
@@ -33,16 +28,6 @@ export class BottleneckFactory {
         this.logger.log(`${contextName}: Rate limit reservoir depleted - queuing requests`);
       }
     });
-
-    // Log queue size when requests are queued (only when log level is debug)
-    if (this.isDebugEnabled) {
-      limiter.on('queued', () => {
-        const queueCount = limiter.counts().QUEUED;
-        if (queueCount > 1) {
-          this.logger.debug(`${contextName}: Rate Limit Queue size reached ${queueCount} requests`);
-        }
-      });
-    }
 
     // Log dropped requests (queue overflow)
     limiter.on('dropped', () => {
