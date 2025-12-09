fb17af42ccf5c581b40bbefe40c4780279f9c46b
Lorand Sandor <lorand@Lorands-MBP.localdomain>
2025-12-09 15:16:18 +0100

    chore: updated coverage list

--- services/sharepoint-connector/vitest.coverage.config.ts
+++ services/sharepoint-connector/vitest.coverage.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       enabled: true,
-      reporter: ['text', 'text-summary', 'html', 'json', 'json-summary', 'lcov'],
+      reporter: ['text', 'json-summary'],
       reportsDirectory: './coverage',
       include: ['src/**/*.ts'],
       exclude: [
