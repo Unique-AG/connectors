ea968a05a4d0fe234f3e807a72704de2c6c98a08
unique-dominik <dominik@unique.ch>
2026-01-08 10:29:40 +0100

    fix(teams-mcp): initial infra deploy fixes

--- services/teams-mcp/deploy/terraform/azure/teams-mcp-entra-application/outputs.tf
+++ services/teams-mcp/deploy/terraform/azure/teams-mcp-entra-application/outputs.tf
@@ -7,10 +7,10 @@ output "client_secrets" {
   description = "Map of client secret and their corresponding Key Vault secrets."
   value = {
     for k, v in var.confidential_clients : k => {
-      client_secret_id       = azuread_application_password.client_secret[k].id
-      client_secret_end_date = azuread_application_password.client_secret[k].end_date
-      key_vault_secret_id    = azurerm_key_vault_secret.kv_client_secret[k].id
-      key_vault_secret_name  = azurerm_key_vault_secret.kv_client_secret[k].name
+      client_secret_end_date                   = azuread_application_password.client_secret[k].end_date
+      client_secret_id                         = azuread_application_password.client_secret[k].id
+      key_vault_secret_name                    = azurerm_key_vault_secret.kv_client_secret[k].name
+      key_vault_secret_resource_versionless_id = azurerm_key_vault_secret.kv_client_secret[k].resource_versionless_id
     }
   }
 }
