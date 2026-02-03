2ce7a5f3a5641300a1b0afe04954c0beea5bee13
Michał Simka <michal.simka@unique.ch>
2026-02-03 09:10:33 +0100

    feat(sharepoint-connector): add GetTopFolderPermissionsQuery
    - Aggregate group permissions from files/dirs under site and library scopes
    - Filter out user permissions, only groups are aggregated
    - Site scope gets all groups, library scope gets groups from that library only

---
+++ services/sharepoint-connector/src/permissions-sync/get-top-folder-permissions.query.ts
@@ -0,0 +1,99 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { filter, isNullish, map, pipe, unique } from 'remeda';
+import {
+  AnySharepointItem,
+  SharepointContentItem,
+  SharepointDirectoryItem,
+} from '../microsoft-apis/graph/types/sharepoint-content-item.interface';
+import { normalizeSlashes } from '../utils/paths.util';
+import { buildIngestionItemKey, getUniquePathFromItem } from '../utils/sharepoint.util';
+import { GroupMembership, Membership } from './types';
+import { groupDistinctId, isTopFolder } from './utils';
+
+interface Input {
+  items: SharepointContentItem[];
+  directories: SharepointDirectoryItem[];
+  permissionsMap: Record<string, Membership[]>;
+  rootPath: string;
+}
+
+@Injectable()
+export class GetTopFolderPermissionsQuery {
+  private readonly logger = new Logger(this.constructor.name);
+
+  public run(input: Input): Map<string, GroupMembership[]> {
+    const { items, directories, permissionsMap, rootPath } = input;
+
+    const topFolders = this.identifyTopFolders(directories, rootPath);
+    const result = new Map<string, GroupMembership[]>();
+
+    for (const topFolderPath of topFolders) {
+      const aggregatedGroups = this.aggregateGroupPermissions(
+        topFolderPath,
+        items,
+        directories,
+        permissionsMap,
+        rootPath,
+      );
+      result.set(topFolderPath, aggregatedGroups);
+    }
+
+    return result;
+  }
+
+  private identifyTopFolders(directories: SharepointDirectoryItem[], rootPath: string): string[] {
+    const normalizedRootPath = `/${normalizeSlashes(rootPath)}`;
+
+    return pipe(
+      directories,
+      map((directory) => getUniquePathFromItem(directory, rootPath)),
+      filter((folderPath) => isTopFolder(folderPath, rootPath)),
+      (paths) => [normalizedRootPath, ...paths],
+      unique(),
+    );
+  }
+
+  private aggregateGroupPermissions(
+    topFolderPath: string,
+    items: SharepointContentItem[],
+    directories: SharepointDirectoryItem[],
+    permissionsMap: Record<string, Membership[]>,
+    rootPath: string,
+  ): GroupMembership[] {
+    const groupsMap = new Map<string, GroupMembership>();
+    const allItems: AnySharepointItem[] = [...items, ...directories];
+
+    for (const item of allItems) {
+      const itemPath = getUniquePathFromItem(item, rootPath);
+
+      if (!this.isDescendantOf(itemPath, topFolderPath)) {
+        continue;
+      }
+
+      const permissionsKey = buildIngestionItemKey(item);
+      const permissions = permissionsMap[permissionsKey];
+
+      if (isNullish(permissions)) {
+        this.logger.warn(`No SharePoint permissions found for item with key ${permissionsKey}`);
+        continue;
+      }
+
+      for (const permission of permissions) {
+        if (permission.type === 'user') {
+          continue;
+        }
+
+        const distinctId = groupDistinctId(permission);
+        if (!groupsMap.has(distinctId)) {
+          groupsMap.set(distinctId, permission);
+        }
+      }
+    }
+
+    return Array.from(groupsMap.values());
+  }
+
+  private isDescendantOf(itemPath: string, topFolderPath: string): boolean {
+    return itemPath === topFolderPath || itemPath.startsWith(`${topFolderPath}/`);
+  }
+}
