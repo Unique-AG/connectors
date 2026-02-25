23245c2be555cf21758a48c6c97f62a1c7dad747
Nicolae Bacila <nicolae.bacila@unique.ai>
2026-02-25 09:35:28 +0200

    Fix query

--- packages/unique-api/src/content/content.queries.ts
+++ packages/unique-api/src/content/content.queries.ts
@@ -10,56 +10,56 @@ export interface GetContentByIdQueryInput {
 }
 
 export const GET_CONTENT_BY_ID_QUERY = gql`
-    query GetContentById {
-        getContentById($contentId: String!) {
-            appliedIngestionConfig
-            byteSize
-            companyId
-            createdAt
-            createdBy
-            deletedAt
-            description
-            expiredAt
-            expiresAt
-            expiresInDays
-            externalFileOwner
-            fileAccess
-            fileAccessState
-            id
-            ingestionConfig
-            ingestionProgress
-            ingestionState
-            ingestionStateDetails
-            ingestionStateUpdatedAt
-            internallyStoredAt
-            key
-            metadata
-            mimeType
-            ownerId
-            ownerType
-            pdfPreviewWriteUrl
-            previewPdfFileName
-            title
-            updatedAt
-            url
-            writeUrl
-            chunks {
-                companyId
-                contentId
-                createdAt
-                createdBy
-                embedding
-                embeddingsFirst10
-                endPage
-                id
-                model
-                order
-                startPage
-                text
-                updatedAt
-                vectorId
-            }
-            readUrl
-        }
+  query GetContentById($contentId: String!) {
+    getContentById(contentId: $contentId) {
+      appliedIngestionConfig
+      byteSize
+      companyId
+      createdAt
+      createdBy
+      deletedAt
+      description
+      expiredAt
+      expiresAt
+      expiresInDays
+      externalFileOwner
+      fileAccess
+      fileAccessState
+      id
+      ingestionConfig
+      ingestionProgress
+      ingestionState
+      ingestionStateDetails
+      ingestionStateUpdatedAt
+      internallyStoredAt
+      key
+      metadata
+      mimeType
+      ownerId
+      ownerType
+      pdfPreviewWriteUrl
+      previewPdfFileName
+      title
+      updatedAt
+      url
+      writeUrl
+      chunks {
+        companyId
+        contentId
+        createdAt
+        createdBy
+        embedding
+        embeddingsFirst10
+        endPage
+        id
+        model
+        order
+        startPage
+        text
+        updatedAt
+        vectorId
+      }
+      readUrl
     }
+  }
 `;
