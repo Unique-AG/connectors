0ad5c3172b5f992cd8bb90c96d502c0e199ffb65
Lorand Sandor <>
2026-05-12 17:41:14 +0300

    chore: DELETED DEPLOY FOLDER

--- services/kyckr-mcp/deploy/helm-charts/kyckr-mcp/templates/_config-app.tpl
+++
@@ -1,7 +0,0 @@
-{{- define "chart.config.app" -}}
-{{- with .Values.mcpConfig }}
-{{- if .app.bufferLogs }}
-APP_BUFFER_LOGS: {{ .app.bufferLogs | quote }}
-{{- end }}
-{{- end }}
-{{- end }}
--- services/kyckr-mcp/deploy/terraform/azure/kyckr-mcp-secrets/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "manual_secrets" {
-  description = "List of manual secrets with name and resource ID."
-  value = [
-    for k, v in azurerm_key_vault_secret.manual_secret : {
-      name = v.name
-      id   = v.id
-    }
-  ]
-}
