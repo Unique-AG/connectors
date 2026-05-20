de4b69085657f67977fbbe7dbf9c092a9e996d86
Nicolae Bacila <nicolae.bacila@unique.ai>
2026-05-20 10:09:59 +0300

    Fix styling

--- services/outlook-semantic-mcp/src/features/post-authorization-user-flow/post-authorization-user-flow.module.ts
+++ services/outlook-semantic-mcp/src/features/post-authorization-user-flow/post-authorization-user-flow.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { InboxDeletingQueryModule } from '../delete-inbox/inbox-deleting-query.module';
 import { DirectoriesSyncModule } from '../directories-sync/directories-sync.module';
 import { SubscriptionModule } from '../subscriptions/subscription.module';
 import { PostAuthorizationListener } from './post-authorization.listener';
-import { InboxDeletingQueryModule } from '../delete-inbox/inbox-deleting-query.module';
 
 @Module({
   imports: [ConfigModule, SubscriptionModule, DirectoriesSyncModule, InboxDeletingQueryModule],
