e3cdd59f165b112fde08a8fa08808c26d26d8042
Nicolae Bacila <nicolae.bacila@unique.ai>
2026-08-12 09:44:49 +0300

    Task 1&3 fixes

---
+++ packages/proxy/src/index.ts
@@ -0,0 +1,9 @@
+export type {
+  BasicProxyConfig,
+  NoAuthProxyConfig,
+  NoneProxyConfig,
+  ProxyConfig,
+  ProxyConfigNamespaced,
+  TlsProxyConfig,
+} from './proxy.config';
+export { ProxyConfigSchema, proxyConfig } from './proxy.config';
